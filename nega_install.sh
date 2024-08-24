c79273ab6bd7c5e11aa8371a1d671ea7b1f90cfd
NEGAX-ADMIN <jsa65x@gmail.com>
2024-08-24 03:57:04 +0200

    Update nega_install.sh
    updating it bro

--- nega_install.sh
+++ nega_install.sh
@@ -93,9 +93,20 @@ echo "==============================================="
 echo "Please enter your License Key, License ID, and HWID as required by the bot, and press Enter."
 echo "Once the download is complete, the script will extract and set up the application."
 
-# Step 7: Extract the downloaded NEGAX_SUPREME file and delete the zip
-chmod +x ./NEGAX_SUPREME_1.5.12.zip
-sudo unzip NEGAX_SUPREME_1.5.12.zip
+# Step 7: Check for template_folder and sender_output directories
+echo "==============================================="
+echo "Step 7: Checking for existing directories..."
+echo "==============================================="
+
+if [ -d "template_folder" ] && [ -d "sender_output" ]; then
+  echo "Both template_folder and sender_output directories exist."
+  echo "Only extracting nega from the ZIP file..."
+  sudo unzip -j NEGAX_SUPREME_1.5.12.zip "nega" -d .
+else
+  echo "One or both of the required directories are missing."
+  echo "Performing a full extraction..."
+  sudo unzip NEGAX_SUPREME_1.5.12.zip
+fi
 
 # Make the nega script executable
 chmod +x ./nega
